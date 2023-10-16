1feaf01d236de001257132ade9be8d548021a728
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2023-10-16 22:21:23 +0530

    api-updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
-import { Route, Switch } from "react-router-dom";
-import Login from "./components/Login";
-import Products from "./components/Products";
-import Checkout from "./components/Checkout";
-import Thanks from "./components/Thanks";
-import Protected from "./components/Protected";
-import NotFound from "./components/NotFound";
+import Register from './components/Register';
+import ipConfig from './ipConfig.json';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Products from './components/Products';
+import Checkout from './components/Checkout';
+import Thanks from './components/Thanks';
+import Protected from './components/Protected';
+import NotFound from './components/NotFound';
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://qkrt-frontend.onrender.com/api/v1`,
 };
 
 function App() {
